22234e3fcad82c79a17e26adda3b6f2efe6cc568
d1pefe <113125565+d1pefe@users.noreply.github.com>
2022-11-05 11:53:56 +0300

    all right, w\o task 6

---
+++ js/index.js
@@ -0,0 +1,64 @@
+// task 1
+let x = 20
+let y = 58
+let z = 42
+
+console.log(x+y+z)
+
+// task 2
+const secInMinute = 60
+const minInHours = 60
+const hoursInDay = 24
+const dayInYear = 365
+
+// task 3
+let count = 42
+let userName = '42'
+
+let countString = String(count)
+let userNameNumber = Number(userName)
+
+//task 4
+let a = 1
+let b = 2
+let c = "белых медведей"
+
+console.log(String(a)+String(b)+String(' ')+String(c))
+
+//task 5
+
+let dost = 'доступ'
+let morp = 'морпех'
+let nal = 'наледь'
+let poper = 'поперек'
+let rubil = 'рубило'
+
+const lengthWords = dost.length + morp.length + nal.length + poper.length + rubil.length
+console.log(lengthWords)
+
+//task 6???
+
+let test1 = 12 
+let test2 = '12'
+let test3 = false
+
+// console.log('Variable: test1 have type:' typeof test1)
+// console.log('Variable: test2 have type:' typeof test2)
+// console.log('Variable: test3 have type:' typeof test3)
+
+//task 7
+
+let usersName = prompt('Укажите ваше имя.', 'Василек')
+let usersAge = prompt('Укажите свой возраст.', 100)
+
+console.log(usersName ,usersAge )
+
+// ADVANCED level
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+let cipher = (codeWord1[1],codeWord2[1],codeWord3[1],codeWord4[1],codeWord5[1])
