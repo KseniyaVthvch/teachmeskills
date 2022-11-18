6876c6cc22ed549fe3383432f0892480ccdbdda3
Yan Skvoroda <113111390+Yan787@users.noreply.github.com>
2022-11-18 15:12:41 +0300

    [HW-2] Yan (#32)
    * hw2-js
    
    * hw2--js
    
    * init.index.js
    
    * init index.js
    
    * init.js
    
    * hw-2 task6 fix

---
+++ index.js
@@ -0,0 +1,80 @@
+//Task 1 
+
+const x = 20
+const y = 58
+const z = 42
+
+console.log(x + y + z)
+
+//Task 2
+
+const minutes = 60
+const hours = 60
+const days = 24
+const years = 365
+
+let myAgeInSeconds = 630720000
+
+console.log(minutes*hours*days*years*myAgeInSeconds)
+//Task 3
+
+const count = 42
+const userName = '42'
+
+console.log(typeof String(count))
+console.log(typeof Number(userName))
+
+//Task 4
+
+let a = 1
+let b = 2
+let c = "белых медведей"
+
+console.log(a + String(b)+ c)
+
+//Task 5 
+
+let aa = "доступ"
+let bb = "морпех"
+let ss = "наледь"
+let xx = "попрек"
+let zz = "рубило"
+
+let lengthWords = aa.length + bb.length + ss.length + xx.length + zz.length
+
+console.log(lengthWords)
+
+//Task 6 
+
+const string = "морпех"
+const number = 43
+const boolean = false
+
+console.log(`Variable:` + string + ` have type:` + typeof string)
+console.log(`Variable:` + number + ` have type:` + typeof number)
+console.log(`Variable:` + boolean + ` have type:` + typeof boolean)
+
+//Task 7
+
+alert(`Hello`);
+let name = prompt("NAME");
+let age = prompt('age?', 18);
+alert(age)
+const info = `name:${name}
+age:${age}`
+console.log(info)
+
+//ADVANCED level
+
+//В данном послании скрыт шифр! Чтобы его разгадать нам нужно соединить все вторые символы слов!
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+const cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]
+
+console.log(cipher)
+//Создайте переменную **`cipher`** и поместите туда необходимые символы
