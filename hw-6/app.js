71eb926e914a6f15062af4b2d81fe0ac34c169da
Mikhail5995 <mikhailgrishko5@yandex.ru>
2022-12-06 18:16:11 +0300

    hw-6

---
+++ hw-6/app.js
@@ -0,0 +1,15 @@
+//1
+
+const colors = ['red', 'green', 'blue']
+console.log(colors.length)
+
+//2
+
+const animals = ['monkey', 'dog', 'cat']
+console.log(animals[animals.length - 1])
+
+//3
+
+const numbers = [5, 43, 63, 23, 90]
+numbers.splice(0, 5)
+console.log(numbers)
