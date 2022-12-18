c875e7000ad3e882d97cb8ba0784794ade3db4cd
polinkasmol <paulinesmolskaya@gmail.com>
2022-12-18 21:23:05 +0300

    decided homowork 7

--- JS-HW-7/app.js
+++ JS-HW-7/app.js
@@ -8,5 +8,30 @@
 // }
 // viewArray(fibonacci)
 
-// Task 2
-const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+// // Task 2
+// const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+// function getStr(arr) {
+// 	return arr.map((item, id) => `member ${id + 1}: ${item}`)
+// }
+
+// console.log(getStr(users))
+
+// // Task 3
+// const numbers = [7, -4, 32, -90, 54, 32, -21]
+// const positiveNumbers = numbers.filter(item => item >= 0)
+// console.log(positiveNumbers)
+
+// // Task 4
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+// let result = fibonacci.reduce((sum, current) => sum + current, 0);
+// console.log(result)
+
+// // Task 5
+// const numbers = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
+// function findNum(arr) {
+// 	return arr.find(item => item % 2 === 0)
+// }
+// console.log(findNum(numbers))
+
+// ADVANCED level
+// Task 1
